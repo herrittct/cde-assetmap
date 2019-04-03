1878275cda43c492fa54b165df96f054d639e5f7
Cam Herritt <cherritt@gmail.com>
2019-04-02 22:38:58 -0600

    got it working with geocoding for all districts
    Signed-off-by: Cam Herritt <cherritt@gmail.com>

--- import.R
+++ import.R
@@ -2,13 +2,21 @@
 library(readxl)
 library(tidyverse)
 library(dplyr)
+library(stringr)
+
+# Get the CDE provided spreadsheet into R
 
 profiles <- read_excel("./data/SFA_profile_Feb_2019.xls", 
                        col_types = c("text", "text", "text","logical", 
                                      "numeric","numeric", "numeric", "logical",
                                      "logical", "logical", 
-                                     "logical","logical")) %>% 
-  dplyr::rename(sfa_number = "Sponsor #", 
+                                     "logical","logical"))
+
+# Modify the standard excell with updated variable names
+# Create a variety of new variables
+
+profiles <- profiles %>% 
+  dplyr::rename(CDE_AGREEMENT = "Sponsor #", 
          day4 = "4 Day School Week",
          student_count = "Pupil Count",
          free_perc = "Free %",
@@ -20,97 +28,38 @@ profiles <- read_excel("./data/SFA_profile_Feb_2019.xls",
          sfsp = "SFSP",
          sfa_name = "Sponsor Name") %>% 
   mutate(sfa_name = str_to_lower(sfa_name)) %>% 
-  mutate(free_students = free_perc * student_count) %>% 
-  mutate(redu_students = redu_perc * student_count) %>% 
-  mutate(free_and_red_perc = free_perc + redu_perc) %>% 
-  mutate(prog_count = 0) %>% 
-  mutate(prog_opportunity = 0) %>% 
-  mutate(sfa_name_short7 = substr(sfa_name, 1, 7)) %>% 
-  mutate(sfa_name_short4 = substr(sfa_name, 1, 4)) %>% 
-  mutate(sfa_name_short10 = substr(sfa_name, 1, 10))
+  mutate(free_students = free_perc * student_count) %>% # count of free students
+  mutate(redu_students = redu_perc * student_count) %>% # count of redu students
+  mutate(free_and_red_perc = free_perc + redu_perc) # combined free & redu
+
 
 # The above actually takes the raw excel into a semi-usable dataframe
 profiles <- filter(profiles, Profile == "Public")
 
+# Import all the schools with GEOID!
+# Must have current directory above data
 
-#profiles <- mutate(profiles, "prog_opportunity" = if (count))
+site_all <- read_excel("./data/CDE_to_GEOID.xlsx", col_types = "text") %>% 
+  mutate(GEOID = ACTUAL_GEOID) %>% 
+  select(GEOID, CDE_AGREEMENT, everything(), -ACTUAL_GEOID)
 
-profiles$prog_count <-  
-  if (profiles["sbp"] == TRUE){
-    mutate(profiles, prog_count = prog_count + 1)
-  }
-  
-# I want to specify a set of variables that I anticipate calling a number of times
-#programs <- filter(profiles, sbp == TRUE) %>% 
-#  filter(nslp == TRUE)
+site_all <- group_by(site_all,
+                      CDE_AGREEMENT)
 
+# Gets all the districts with their site count and GEOID
+(districts <- summarise(site_all, 
+                       count_schools = n(),
+                       GEOID = first(GEOID)))
 
-# What I want to do is calculate the number of programs they are doing
-for i in programs
-if smp == TRUE {
-  mutate(profiles$prog_count + 1)
-} else {
-  mutate(fofiles$prog_opportunity +1)
-  }
+# Combines districts with GEOID number
+geoid_districts <- left_join(districts, profiles, by = "CDE_AGREEMENT")
 
-profiles %>% 
-  group_by(free_perc > 0.85)
-# I do not want to filter out these districts
-programs <- profiles[sbp, nslp, smp, snack, sfsp]
+### This WORKS!!
+combined_geo_dist <- left_join(co_school_joined, geoid_districts, by = "GEOID")
 
-programs_present <- filter(profiles, programs %in% TRUE)
+plot(combined_geo_dist["nslp"], main = "NSLP Updated")
+
+# I am thinking that there is something to do with how the df is getting merged 
+# that is making it lose the ability to map, no idea why though. 
 
 
-#four_day <- filter(profiles, day4 == "X")
-four_day <- filter(profiles, day4 == TRUE)
-four_day  
-dim(four_day)
-has_nslp <- filter(profiles, nslp == "X")
-has_nslp
-
-has_smp <- filter(profiles, smp == "X")
-has_smp
-
-
-profiles
-head(profiles)
-profiles$sbp <- mutate(profiles, sbp = as_factor(sbp,))
-profiles
-  
-day4.sfa <- filter(profiles, "4 Day School Week")
-day4.sfa
-
-class("Breakfast")
-
-
-
-library(tidyverse)
-library(sf)
-library(tigris)
-options(tigris_class = "sf")
-
-#get all colorado school districts shape geometry etc. from the tigris package ----
-colorado_school_districts <- 
-  school_districts(state = "CO",
-                   type = "unified")
-
-#write it a file for use anytime and so it doesn't have to be continually downloaded for the report ----
-write_rds(colorado_school_districts, "colorado_school_districts.rds")  
-
-#there is about a billion, roughly, ways to go about this, here is one simple one. 
-
-colorado_school_districts %>% 
-  ggplot(aes(fill = str_detect(NAME, params$district))) +
-  scale_fill_manual(values = c("white", "darkgreen")) +
-  geom_sf() +
-  guides(fill = "none") +
-  labs(title = str_c(params$district, " Unified School District")) +
-  theme_minimal()
-
-
-head(colorado_school_districts)
-dim(colorado_school_districts)
-union(colorado_school_districts, profiles)
-dim(union(colorado_school_districts, profiles))
-
-union(colorado_school_districts,profiles)
--- mapping.r
+++ mapping.r
@@ -27,37 +27,6 @@ co_school_joined <- left_join(colorado_school,
                               colorado_income,
                               by = "GEOID")
 
-co_school_joined <- dplyr::rename(co_school_joined, 
-                           sfa_name = "NAME.x") %>% 
-  mutate(sfa_name = str_to_lower(sfa_name))
-
-co_school_joined <-
-  mutate(co_school_joined, sfa_name_short7 = substr(sfa_name,1,7))
-
-# Not currently using the below substrings but may need to refine and use later
-co_school_joined <-
-  mutate(co_school_joined, sfa_name_short10 = substr(sfa_name,1,10))
-
-co_school_joined <-
-  mutate(co_school_joined, sfa_name_short4 = substr(sfa_name,1,4))
-  
-# Trying to add in data from our CDE information
-# THIS ONE WORKS!! For most of the districts 167/178
-super_df <- inner_join(co_school_joined,
-                       profiles,
-                       by = "sfa_name_short7")
-
-
-# This one causes problems but points out another problem regarding non-pub
-super_df4 <- inner_join(co_school_joined,
-                        profiles,
-                        by = "sfa_name_short4")
-
-
-super_df10 <- inner_join(co_school_joined,
-                        profiles,
-                        by = "sfa_name_short10")
-
 
 # Plot out using GIS all the different SFAs with whatever data we want to show
 
@@ -76,10 +45,4 @@ plot(super_df["student_count"], main= "Number of Students")
 
 # What I need to work on is taking the names apart and trying to match them automatically
 # Create a new variable in profiles from the geoID so that all the districts match
-
-head(co_school_joined)
-
-
-
-
 
